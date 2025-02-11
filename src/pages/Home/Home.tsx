2c4ad35f26a8b50d874773801d1882e4f9085b9c
Tanya <kolt.levry@gmail.com>
2025-02-11 14:41:07 +0100

    start wright input logic

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,28 @@
 import Button from "components/Button/Button";
 import Input from "components/Input/Input";
 import { MainhBox, Resultbox, SearchBox } from "./styles";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
 function Home() {
+
+    const [city, setCity] = useState<string>('');
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCity(event.target.value);
+    };
+
+    const dispatch = useDispatch();
+
+    const handleSearch = () => {
+        if(!city.trim()) return;
+        dispatch(fetchWeather(city));
+    }
+
     return (
         <MainhBox>
             <SearchBox>
-                <Input placeholder="Enter city name"></Input>
+                <Input placeholder="Enter city name" onChange={handleInputChange}></Input>
                 <Button name="Search"></Button>
             </SearchBox>
             <Resultbox>
